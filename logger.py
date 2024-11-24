f181e60a11eca01d8fb3aebbfde3b931f4666902
Jason Merrone <jasonmerrone@gmail.com>
2024-11-23 20:09:04 -0700

    Minor logger updates

--- logger.py
+++ logger.py
@@ -1,10 +1,19 @@
 import logging
+from logging.handlers import RotatingFileHandler
 from pythonjsonlogger import jsonlogger
 
 def setup_logging():
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    log_handler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter('%(asctime)s %(name)s %(levelname)s %(message)s')
-    log_handler.setFormatter(formatter)
-    logger.addHandler(log_handler)
+
+    # File handler with rotation
+    file_handler = RotatingFileHandler('consumer.log', maxBytes=5_000_000, backupCount=3)
+    file_formatter = jsonlogger.JsonFormatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
